6506f95a225b1153c0276663c3a0864cf1116408
andrew <fgtrjty@dfhfj.ru>
2018-05-23 15:05:58 +0500

    prepared for deploy

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 const Users = require('./users');
 
 const publicPath = path.join(__dirname, '../public');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
